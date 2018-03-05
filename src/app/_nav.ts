42e27b83c4aa0e1a51b13fa5a30f6bc5084c2a76
Wang <wange@ad.tri.sbc.com>
2018-03-05 14:14:36 -0800

    Change footer

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -2,10 +2,36 @@ export const navigation = [
   {
     name: 'Dashboard',
     url: '/dashboard',
-    icon: 'icon-speedometer',
-    // badge: {
-    //   variant: 'info',
-    //   text: 'NEW'
-    // }
-  }
+    icon: 'icon-speedometer'
+  },
+  {
+    title: true,
+    name: 'Main Menu'
+  },
+  {
+    name: 'Schedule',
+    url: '',
+    icon: 'icon-star',
+    children: [
+      {
+        name: 'Calendar',
+        url: '/',
+        icon: 'icon-calendar'
+      },
+      {
+        name: 'Projects',
+        url: '/',
+        icon: 'icon-puzzle'
+      },
+      {
+        name: 'Timeline',
+        url: '/',
+        icon: 'icon-clock'
+      },
+      {
+        name: 'Status',
+        url: '/',
+        icon: 'icon-graph'
+      },
+    ]}
 ];
