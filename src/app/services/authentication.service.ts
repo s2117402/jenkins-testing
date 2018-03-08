9b672df469d749dab8f7ec68c1f3b317bd89543f
Wang <wange@ad.tri.sbc.com>
2018-03-08 13:39:10 -0800

    nothing

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,31 +1,15 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpInterceptor} from '@angular/common/http';
-import {LoginUser} from '../entities/Request';
-
-import {Observable} from 'rxjs/Observable';
-import { map } from 'rxjs/operators';
-
-import {ResponseOfAuthenticatingJWT, ResponseWithJWT} from '../entities/Response';
-import {HttpInterceptingHandler} from '@angular/common/http/src/module';
-import {HttpRequest, HttpHandler, HttpEvent} from '@angular/common/http';
-import {JsonWebTokenService} from './json-web-token.service';
 
 @Injectable()
 export class AuthenticationService {
-  //private  RootURL = 'https://attserver.herokuapp.com/';
-  private RootURL = 'http://localhost:3000/';
-  logUserName;
-  constructor(
-    private http: HttpClient,
-    private jwtService: JsonWebTokenService
-  ) { }
+  loggedIn = true;
+  constructor() { }
 
-  login(user: LoginUser): Observable<ResponseWithJWT> {
-    return this.http.post<ResponseWithJWT>(`${this.RootURL}login`, user);
-  }
-
-  docheck(): Observable<any> {
-    return this.http.get<ResponseOfAuthenticatingJWT>
-      (`${this.RootURL}check`);
+  getLoginStatus() {
+    if (localStorage.getItem('JWT')) {
+      return localStorage.getItem('JWT');
+    } else {
+      return '';
+    }
   }
 }
