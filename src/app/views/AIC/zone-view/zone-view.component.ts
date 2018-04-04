64d718529f38fd301403c3b7e326077cce2d9802
Sun, Will (ws0155) <ws0155@att.com>
2018-04-04 13:29:57 -0500

    added table

--- src/app/views/AIC/zone-view/zone-view.component.ts
+++ src/app/views/AIC/zone-view/zone-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {RequestDataService} from '../../../services/request-data.service';
 
 @Component({
   selector: 'app-zone-view',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ZoneViewComponent implements OnInit {
 
-  constructor() { }
+
+  allZones = [];
+  constructor(
+    private requestDataService: RequestDataService
+  ) { }
+
 
   ngOnInit() {
+    this.requestDataService.getTenant().subscribe(res=> {
+      this.allZones = res['zones'];
+      console.log(this.allZones);
+    })
   }
 
 }
